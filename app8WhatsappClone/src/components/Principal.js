fd6361dda856527032f30c0bbb8fede3d8975ada
Paulo-Sergio <paulofranca.contato@gmail.com>
2019-06-22 15:42:27 -0300

    trabalhando com tabview e criando components/Contatos.js / components/Conversas.js

--- app8WhatsappClone/src/components/Principal.js
+++ app8WhatsappClone/src/components/Principal.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TabViewAnimated, SceneMap } from 'react-native-tab-view';
 
-const Principal = (props) => (
-  <View style={{ marginTop: 100 }}>
-    <Text>Página principal da aplicação</Text>
-  </View>
-)
+import TabBarMenu from './TabBarMenu';
+import Conversas from './Conversas';
+import Contatos from './Contatos';
 
-export default Principal;
+export default class Principal extends Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: '1', title: 'Conversas' },
+      { key: '2', title: 'Contatos' }
+    ],
+  };
+
+  _handleChangeTab = index => this.setState({ index });
+
+  _renderHeader = props => <TabBarMenu {...props} />;
+
+  _renderScene = SceneMap({
+    '1': Conversas,
+    '2': Contatos
+  });
+
+  render() {
+    return (
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onRequestChangeTab={this._handleChangeTab}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
